31499d35a45ee3b500e9915edc5a1e7c4d82582b
Robin Krom <robin.krom@math.ethz.ch>
2013-06-04 12:09:49 +0200

    cleanout: removed tests/ some cosmetics.

--- src/MultiTimeSeries.hs
+++ src/MultiTimeSeries.hs
@@ -40,69 +40,45 @@ import qualified Data.Packed as P
 import qualified Numeric.Container as C
 import qualified Numeric.LinearAlgebra as LA
 import qualified Data.Vector.Generic as G
+import qualified Data.Vector as V
+import qualified Data.List.Zipper as Z
 import Statistics.Distribution (Distribution, cumulative)
 import Statistics.Distribution.ChiSquared
 import Statistics.Test.Types (TestResult(..), significant)
-import Data.List (minimumBy)
+import Data.List
 import Data.Ord (comparing)
 import Data.Complex (magnitude)
+import Data.Function (on)
 
 type Vector = P.Vector Double
 type Matrix = P.Matrix Double
-type MSample = [Vector]
-type MWeightedSample = [(Double, Vector)]
+type Sample = [Vector]
+type WeightedSample = [(Double, Vector)]
 type VarModel = (Vector, [Matrix]) -- ^ (\phi_0, matrices \Phi_1 ... \Phi_p).
 
--- | Rounding error upper bound. Can get terrible if matrices are of bad shape in tests.
-roundingError :: Double 
-roundingError = 1e-7
 
 -- | Estimates the mean of a sample.
-mean :: MSample -> Vector
-mean s | badMSample s = undefined
+mean :: Sample -> Vector
 mean s = C.scale (1 / fromIntegral (length s)) (sum s)
 
-prop_mean_invariant_under_reversion :: MSample -> Bool
-prop_mean_invariant_under_reversion s = C.norm1 (mean s - mean (reverse s)) < roundingError
-
-prop_mean_linear :: (Double, MSample, MSample) -> Bool
-prop_mean_linear (mu, s1, s2) = C.norm1 (C.scale mu (mean s1) + mean s2 - mean (zipWith (+) (map (C.scale mu) s1) s2)) < roundingError
-
 -- | Estimates the crosscovariance matrix.
-crosscovariance :: MSample -> MSample -> Matrix
-crosscovariance s1 s2 | badMSample s1 || badMSample s2 = undefined
+crosscovariance :: Sample -> Sample -> Matrix
 crosscovariance s1 s2 = C.scale (1 / fromIntegral (length s1)) (sum $ zipWith C.outer s1 s2)
-
-prop_ccv_switch_args :: (MSample, MSample) -> Bool
-prop_ccv_switch_args (s1, s2) = C.norm1 (P.flatten (crosscovariance s1 s2 - P.trans (crosscovariance s2 s1))) < roundingError
-
--- terrible rounding errors.
-prop_ccv_sum :: (MSample, MSample) -> Bool
-prop_ccv_sum (s1, s2) = C.norm1 (P.flatten (crosscovariance s s - crosscovariance s1 s1 - crosscovariance s1 s2 - crosscovariance s2 s1 - crosscovariance s2 s2)) < 10 where s = zipWith (+) s1 s2
 
 -- | Estimates the crossvariance with a certain lag of a sample
 crossvariance :: Int -- ^ Lag.
-                    -> MSample -- ^ The sample.
+                    -> Sample -- ^ The sample.
                     -> Matrix -- ^ Crossvariance matrix.
-crossvariance _ s | badMSample s = undefined
 crossvariance l s = let 
     s' = map (\v -> v - mean s) s
     s'' = drop l s'
     in
     crosscovariance s' s''
 
-prop_cv_symmetric :: MSample -> Bool
-prop_cv_symmetric s = C.norm1 (P.flatten (m - P.trans m)) < roundingError where m = crossvariance 0 s
-
--- terrible rounding errors.
-prop_cv_transformation :: (MSample, Matrix, Vector) -> Bool
-prop_cv_transformation (s, m_A, a) = C.norm1 (P.flatten (crossvariance 0 (map (\x -> m_A C.<> x + a) s) - (m_A C.<> crossvariance 0 s C.<> P.trans m_A))) < 10
-
 -- | Estimates the crosscorrelation with a certain lag of a sample.
 crosscorrelation :: Int -- ^ Lag. 
-                    -> MSample -- ^ The sample.
+                    -> Sample -- ^ The sample.
                     -> Matrix -- ^ Crosscorrelation matrix.
-crosscorrelation _ s | badMSample s = undefined
 -- Check for zero standart deviation.
 crosscorrelation _ s | C.prodElements (sum (map (G.map (^(2 :: Int)) . (\v -> v - mean s)) s)) == 0 = undefined
 crosscorrelation l s = let
@@ -115,9 +91,8 @@ crosscorrelation l s = let
 -- | Multivariate Portmenteau test.
 portmanteauTest :: Int -- ^ Dimension of the vector space. 
                     -> Int -- ^ Maximal lag.
-                    -> MSample -- ^ The sample.
+                    -> Sample -- ^ The sample.
                     -> TestResult
-portmanteauTest _ _ s | badMSample s = undefined
 portmanteauTest dimV _ _ | dimV == 0 = undefined
 portmanteauTest dimV m s = let
     m_Gamma_0_inv = LA.inv $ crosscorrelation 0 s 
@@ -131,9 +106,8 @@ portmanteauTest dimV m s = let
 -- r_t = \phi_0 + \sum_{i=1}^p \Phi_i r_{t-i} + e_t
 varModel :: Int -- ^ Dimension of the vector space.
             -> Int -- ^ p parameter of the model.
-            -> MSample -- ^ The sample.
+            -> Sample -- ^ The sample.
             -> VarModel -- ^ A var(p) model.
-varModel _ _ s | badMSample s = undefined
 varModel dimV p s = let
     l = length s
     -- build matrices as described here:
@@ -164,25 +138,22 @@ varResidual vm  i rs = head rs - varForecast vm (take i (tail rs))
 -- | The residual covariance matrix estimate by osl method.
 oslResidualCovariance :: VarModel
                         -> Int -- ^ The order of the residual
-                        -> MSample -- ^ Sample vec(r_1, r_2, r_3 ... ]
+                        -> Sample -- ^ Sample vec(r_1, r_2, r_3 ... ]
                         -> Matrix
-oslResidualCovariance _ _ s | badMSample s = undefined
 oslResidualCovariance vm i s = C.scale (1/(fromIntegral (length s) - 2* fromIntegral i -1)) (residualCovariance vm i s)
 
 -- | The residual covariance matrix estimate by maximum likelyhood method.
 mlResidualCovariance :: VarModel
                         -> Int -- ^ The order of the residual
-                        -> MSample -- ^ Sample vec(r_1, r_2, r_3 ... ]
+                        -> Sample -- ^ Sample vec(r_1, r_2, r_3 ... ]
                         -> Matrix
-mlResidualCovariance _ _ s | badMSample s = undefined
 mlResidualCovariance vm i s = C.scale (1/fromIntegral (length s)) (residualCovariance vm i s)
 
 -- | Helper function for oslResidualCovariance and mlResidualCovariance.
 residualCovariance :: VarModel -- ^ The var model.
                         -> Int -- ^ The order of the residual
-                        -> MSample -- ^ Sample vec(r_1, r_2, r_3 ... ]
+                        -> Sample -- ^ Sample vec(r_1, r_2, r_3 ... ]
                         -> Matrix
-residualCovariance _ _ s | badMSample s = undefined
 residualCovariance vm i s = let
     l = length s
     a_i t = varResidual vm i (reverse (take t s))
@@ -191,10 +162,9 @@ residualCovariance vm i s = let
 
 -- | Select the var(p) model with the minimal akaike information criterion.
 akaikeInfoCrit :: Int
-                    -> MSample -- ^ The sample.
+                    -> Sample -- ^ The sample.
                     -> Int -- ^ Upper bound for the order of the var model.
                     -> VarModel -- ^ The best var model with minimal aic.
-akaikeInfoCrit _ s _ | badMSample s = undefined
 akaikeInfoCrit dimV s m = let
     aic vm = log (abs (LA.det $ mlResidualCovariance vm i s)) + (2.0 * fromIntegral dimV^(2 :: Int) * fromIntegral i) / fromIntegral (length s) where i = (length $ snd vm)
     in
